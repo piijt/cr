d68d12940e123b9520288588fc7b1afda254944f
piijt <piijt@hotmail.com>
2020-04-16 13:41:28 +0100

    Updates

---
+++ precache-manifest.21a579fd6396975b6e86425e42ea75ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bdb97af6d04b8ad48054204d3ca1b87",
+    "url": "/cr/index.html"
+  },
+  {
+    "revision": "ecc593e0f0675d69c621",
+    "url": "/cr/static/css/main.474dd6b8.chunk.css"
+  },
+  {
+    "revision": "21d8ebc1b43f4840d4db",
+    "url": "/cr/static/js/2.24b89d5c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/cr/static/js/2.24b89d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc593e0f0675d69c621",
+    "url": "/cr/static/js/main.cbbaf48c.chunk.js"
+  },
+  {
+    "revision": "7cbf6a576f26b96cc8fd",
+    "url": "/cr/static/js/runtime-main.24d1cf34.js"
+  }
+]);
